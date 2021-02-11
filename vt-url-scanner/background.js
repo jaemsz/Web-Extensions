029a75c3f3a99072cc033e9d2ce5a3fc9cdc04d6
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-02-10 21:03:31 -0800

    Update background.js

--- vt-url-scanner/background.js
+++ vt-url-scanner/background.js
@@ -4,9 +4,10 @@ VT_URL_SCAN_URL = "https://www.virustotal.com/api/v3/urls";
 VT_URL_ID_SCAN_URL = "https://www.virustotal.com/api/v3/urls/{id}";
 
 VT_GUI_FILE_ID_DETECTION_URL = "https://www.virustotal.com/gui/file/{id}/detection";
-VT_GUI_URL_ID_DETECTION_URL = "https://www.virustotal.com/gui/url/{id}/detection"
+VT_GUI_URL_ID_DETECTION_URL = "https://www.virustotal.com/gui/url/{id}/detection";
 
 ERROR_INVALID_API_KEY = "Please use a valid VirusTotal API key";
+ERROR_ACCESS_DENIED = "VirusTotal cannot access the downloaded file. Please upload it to VirusTotal manually.";
 ERROR_GENERIC = "Please wait 60 seconds and try again...";
 
 let vtApiKey = "";
@@ -28,21 +29,25 @@ browser.storage.onChanged.addListener(changeData => {
     vtApiKey = changeData.vtApiKey.newValue;
 });
 
-function vtScanUrl(url)
+function vtScan(method, url, formData)
 {
-    let p = new Promise(function(resolve, reject) {
-        fd = new FormData();
-        fd.append("url", url);
-        
+    let p = new Promise(function(resolve, reject) {        
         xhr = new XMLHttpRequest();
-        xhr.open("POST", VT_URL_SCAN_URL, true);
+        xhr.open(method, url, true);
         xhr.setRequestHeader(VT_API_KEY_HEADER, vtApiKey);
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200)
             {
                 responseJSON = JSON.parse(xhr.responseText);
-                id = responseJSON.data.id.split("-")[1];
-                resolve(id);
+                if (responseJSON.data.attributes
+                    && responseJSON.data.attributes.last_http_response_code == 403)
+                {
+                    reject(ERROR_ACCESS_DENIED);
+                }
+                else
+                {
+                    resolve(responseJSON);
+                }
             }
             else if (xhr.status == 401)
             {
@@ -53,49 +58,34 @@ function vtScanUrl(url)
                 reject(ERROR_GENERIC);
             }
         }
-        xhr.send(fd);
+        xhr.send(formData);
     });
     
     return p;
 }
 
+function vtScanUrl(url)
+{
+    var fd = new FormData();
+    fd.append("url", url);
+    return vtScan("POST", VT_URL_SCAN_URL, fd);
+}
+
 function vtScanUrlId(urlId)
 {
-    let p = new Promise(function(resolve, reject) {        
-        let vtUrl = VT_URL_ID_SCAN_URL;
-        vtUrl = vtUrl.replace("{id}", urlId);
-        
-        xhr = new XMLHttpRequest();
-        xhr.open("GET", vtUrl, true);
-        xhr.setRequestHeader(VT_API_KEY_HEADER, vtApiKey);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200)
-            {
-                responseJSON = JSON.parse(xhr.responseText);
-                resolve(responseJSON.data);
-            }
-            else if (xhr.status == 401)
-            {
-                reject(ERROR_INVALID_API_KEY);
-            }
-            else if (xhr.status != 200)
-            {
-                reject(ERROR_GENERIC);
-            }
-        }
-        xhr.send();
-    });
-    
-    return p;
+    var vtUrl = VT_URL_ID_SCAN_URL;
+    vtUrl = vtUrl.replace("{id}", urlId);
+    return vtScan("GET", vtUrl, null);
 }
 
 function onQueryActiveTab(tabs)
 {
     let tab = tabs[0];
-    let scanningUrl = vtScanUrl(tab.url);
+    const scanningUrl = vtScanUrl(tab.url);
     
-    scanningUrl.then((id) => {
-        url = VT_GUI_URL_ID_DETECTION_URL;
+    scanningUrl.then((response) => {
+        let id = response.data.id.split("-")[1];
+        let url = VT_GUI_URL_ID_DETECTION_URL;
         url = url.replace("{id}", id);
         browser.tabs.create({
             "url": url
@@ -122,6 +112,11 @@ function onDownloadCreated(item)
 
 function onDownloadChanged(delta)
 {
+    if (delta.url)
+    {
+        console.log(`delta.url = ${delta.url}`);
+    }
+    
     if (delta.state && delta.state.current == "complete")
     {
         let url = downloadUrls[delta.id];
@@ -131,18 +126,26 @@ function onDownloadChanged(delta)
             url = delta.url;
         }
         
-        let scanningUrl = vtScanUrl(url);
+        const scanningUrl = vtScanUrl(url);
         
-        scanningUrl.then((id) => {
-            let scanningUrlId = vtScanUrlId(id);
+        scanningUrl.then((response) => {
+            let id = response.data.id.split("-")[1];            
             
-            scanningUrlId.then((data) => {
-                sha256 = data.attributes.last_http_response_content_sha256;
+            const scanningUrlId = vtScanUrlId(id);
+            
+            scanningUrlId.then((response) => {
+                sha256 = response.data.attributes.last_http_response_content_sha256;
                 url = VT_GUI_FILE_ID_DETECTION_URL;
                 url = url.replace("{id}", sha256);
                 browser.tabs.create({
                     "url": url
                 });
+            }, (error) => {
+                alert_msg = "alert('" + error + "');";
+                const executing = browser.tabs.executeScript({
+                    code: alert_msg
+                });
+                executing.then(() => {});
             });
         }, (error) => {
             alert_msg = "alert('" + error + "');";
